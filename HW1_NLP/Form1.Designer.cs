45c10acc27675c497d43595b81b421a8fcace643
nisanur bulut <nisanurrunasin@hotmail.com>
2018-10-14 20:43:56 +0300

    Ana sayfa iskeleti yazılmaya başlandı

--- HW1_NLP/Form1.Designer.cs
+++ HW1_NLP/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbl0 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tbl0
+            // 
+            this.tbl0.ColumnCount = 1;
+            this.tbl0.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tbl0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbl0.Location = new System.Drawing.Point(0, 0);
+            this.tbl0.Name = "tbl0";
+            this.tbl0.RowCount = 2;
+            this.tbl0.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tbl0.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tbl0.Size = new System.Drawing.Size(800, 450);
+            this.tbl0.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbl0);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tbl0;
     }
 }
 
